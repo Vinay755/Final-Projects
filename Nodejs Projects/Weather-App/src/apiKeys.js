647ef0e35cccf27fa4ec8e8f3bec3e4790534622
Vinay M <vinay@7dliving.com>
2024-05-04 11:34:56 +0530

    updates weather app node application

---
+++ Nodejs Projects/Weather-App/src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "{841ab1bae13ccb47dc7a46e0868a838c}",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
